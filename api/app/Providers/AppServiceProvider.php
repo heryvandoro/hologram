5f971a1b18dc7d7cf3b0d6e66202e3d662ccdc56
Hery Vandoro <vandorohery99@gmail.com>
2017-08-24 18:11:18 +0700

    Fixing timezone and migrate problem

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        date_default_timezone_set('Asia/Jakarta');
+        Schema::defaultStringLength(191);
     }
 
     /**
